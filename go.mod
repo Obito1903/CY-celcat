b7b873bd8193a2221ac8379d156f7f782aae166d
Samuel <sam200133@gmail.com>
2021-04-05 21:33:25 +0200

    v2

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
-module calcat/v1
+module github.com/Obito1903/CY-celcat
 
 go 1.16
 
 require (
-	github.com/arran4/golang-ical v0.0.0-20210113030058-c451667b5431 // indirect
-	golang.org/x/net v0.0.0-20210329181859-df645c7b52b1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
+	github.com/arran4/golang-ical v0.0.0-20210405013514-3a97d5d26366 // indirect
+	github.com/srwiley/oksvg v0.0.0-20210320200257-875f767ac39a // indirect
+	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
+	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c // indirect
 )
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/Obito1903/CY-celcat/celcat"
+	"github.com/Obito1903/CY-celcat/celcat/common"
+	"github.com/Obito1903/CY-celcat/celcat/fetch"
+)
+
+func firstDayOfISOWeek(timezone *time.Location) time.Time {
+	year, week := time.Now().ISOWeek()
+	date := time.Date(year, 0, 0, 0, 0, 0, 0, timezone)
+	isoYear, isoWeek := date.ISOWeek()
+	for date.Weekday() != time.Monday { // iterate back to Monday
+		date = date.AddDate(0, 0, -1)
+		isoYear, isoWeek = date.ISOWeek()
+	}
+	for isoYear < year { // iterate forward to the first day of the first week
+		date = date.AddDate(0, 0, 1)
+		isoYear, isoWeek = date.ISOWeek()
+	}
+	for isoWeek < week { // iterate forward to the first day of the given week
+		date = date.AddDate(0, 0, 1)
+		isoYear, isoWeek = date.ISOWeek()
+	}
+	return date
+}
+
+func main() {
+	var err error
+	genSVG := false
+	svgName := "cal.svg"
+	configPath := "example.config.json"
+	firstDayofWeek := firstDayOfISOWeek(time.UTC)
+	queryPeriod := common.Period{
+		StartDate: firstDayofWeek,
+		EndDate:   firstDayofWeek.Add(time.Hour * 24 * 6),
+	}
+
+	for i, arg := range os.Args {
+		switch arg {
+		case "-c":
+			configPath = os.Args[i+1]
+		case "-d":
+			if len(os.Args) < i+2 {
+				log.Fatal("Arguments manquant")
+				os.Exit(1)
+
+			}
+			loc, _ := time.LoadLocation("Europe/Paris")
+			queryPeriod.StartDate, err = time.ParseInLocation("2006-01-02", os.Args[i+1], loc)
+			common.CheckErr(err)
+			queryPeriod.EndDate, err = time.ParseInLocation("2006-01-02", os.Args[i+2], loc)
+			common.CheckErr(err)
+		case "-svg":
+			genSVG = true
+			if len(os.Args) < i+2 {
+
+			} else {
+				svgName = os.Args[i+1]
+			}
+		}
+
+	}
+
+	config := celcat.GetConfig(configPath)
+	calendar := fetch.GetCalendar(config, queryPeriod)
+	celcat.ToICS(calendar)
+	if genSVG {
+		celcat.ToSVG(calendar, svgName)
+	}
+
+}
