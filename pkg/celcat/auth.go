2bf173926c39d379e805f275d958fff50ed6e11c
Samuel Rodrigues <sam200133@gmail.com>
2022-01-26 15:44:58 +0100

    idk

--- pkg/celcat/auth.go
+++ pkg/celcat/auth.go
@@ -2,6 +2,7 @@ package celcat
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -20,6 +21,7 @@ func getRequestVerificationToken(client *http.Client, url url.URL) string {
 
 	// Init the scanner
 	scanner := bufio.NewScanner(resp.Body)
+
 	// Scan the response for the __RequestVerificationToken element
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), "__RequestVerificationToken") {
@@ -37,6 +39,7 @@ func getRequestVerificationToken(client *http.Client, url url.URL) string {
 func Login(client *http.Client, celcatUrl url.URL, username string, password string) SessionData {
 	var session SessionData
 	session.token = getRequestVerificationToken(client, celcatUrl)
+	fmt.Println("1")
 	session.location = celcatUrl
 
 	// Init the header to be sent
@@ -45,26 +48,42 @@ func Login(client *http.Client, celcatUrl url.URL, username string, password str
 		"Password":                   {password},
 		"__RequestVerificationToken": {session.token},
 	}
+	fmt.Println("2")
+
 	// Setup a login request
 	req, err := http.NewRequest("POST", celcatUrl.String()+"/LdapLogin/Logon", strings.NewReader(formData.Encode()))
 	if err != nil {
 		log.Fatal("Could not create request.", err)
 		os.Exit(1)
 	}
+	fmt.Println("3")
+
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	// Send the login request
 	resp, err := client.Do(req)
+	fmt.Println("4")
 
-	if err != nil || resp.StatusCode != 200 {
+	if err != nil {
 		log.Fatal("Could not login to : ", celcatUrl.String(), err)
 		os.Exit(1)
 	}
+	if resp.StatusCode != 200 {
+		log.Fatal("Could not login to : ", celcatUrl.String())
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp.StatusCode)
+
 	responseUrl, err := resp.Request.Response.Location()
+	fmt.Println("6")
+
 	if err != nil {
 		log.Fatal("Could not parse response.", err)
 		os.Exit(1)
 	}
+	fmt.Println("7")
+
 	session.federationId = responseUrl.Query().Get("FederationIds")
 	return session
 }
