640925e7cd90aab334395a7323c45009a29d58fd
Samuel Rodrigues <sam200133@gmail.com>
2022-02-08 12:52:47 +0100

    html templating

--- pkg/calendar/html/html.go
+++ pkg/calendar/html/html.go
@@ -3,15 +3,48 @@ package html
 import (
 	"log"
 	"os"
+	"time"
+
+	"html/template"
 
 	"github.com/Obito1903/CY-celcat/pkg/calendar"
-	"golang.org/x/net/html"
 )
 
-func CalendarToHtml(cal calendar.Calendar, pathToTemplate string) {
-	file, _ := os.Open(pathToTemplate)
-	doc, err := html.Parse(file)
+type HtmlEvent struct {
+	Event    calendar.Event
+	Top      float32
+	Height   float32
+	TimeSpan string
+}
+
+type HtmlDay struct {
+	Name   string
+	Events []HtmlEvent
+}
+
+type HtmlCalendar struct {
+	Horaires []string
+	Days     []HtmlDay
+	MaxSpan  time.Duration
+}
+
+func CalendarToHtml(cal calendar.Calendar, templatePath string) string {
+	t := template.New("calendar")
+	t, err := template.ParseFiles(templatePath)
 	if err != nil {
-		log.Fatal("Could not open the template.", err)
+		log.Fatal("Could not load template.", err)
+	}
+
+}
+
+func HtmlToFile(htmlCal string, path string) {
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatal("Could not save HTML.", err)
+	}
+	defer f.Close()
+	_, err = f.WriteString(htmlCal)
+	if err != nil {
+		log.Fatal("Could not save HTML.", err)
 	}
 }
